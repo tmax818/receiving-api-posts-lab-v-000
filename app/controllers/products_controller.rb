2645108a5f5e2596a1b05ee45f181b6a620d76f1
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-06 06:15:47 -0700

    init commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
   end
@@ -18,8 +19,8 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
-    redirect_to products_path
+    @product = Product.create(product_params)
+    render json: @product, status: 201
   end
 
   def show
